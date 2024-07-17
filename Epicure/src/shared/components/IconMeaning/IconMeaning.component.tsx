f787bd34016159224aa96abc7387ce8abeb185af
itamarLeiboMoveo <itamarl@moveo.co.il>
2024-07-17 13:43:15 +0300

    fixed pr changes

--- Epicure/src/shared/components/IconMeaning/IconMeaning.component.tsx
+++ Epicure/src/shared/components/IconMeaning/IconMeaning.component.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import data from '../../../data/backend.json';
 
 import './IconMeaning.style.scss';
+import IconContainer from './components/IconContainer/IconContainer.component.tsx';
 
 function IconMeaning() {
     const { icons } = data;
@@ -11,18 +12,9 @@ function IconMeaning() {
         <div className='icon-meaning-container'>
             <h1 className='icon-meaning-title'>THE MEANING OF OUR ICONS:</h1>
             <div className='icons-container'>
-                <div className='icon-container'>
-                    <img className='icon' src={icons[7].image} />
-                    <h1 className='icon-name'>spicy</h1>
-                </div>
-                <div className='icon-container'>
-                    <img className='icon' src={icons[8].image} />
-                    <h1 className='icon-name'>vegeterian</h1>
-                </div>
-                <div className='icon-container'>
-                    <img className='icon' src={icons[9].image} />
-                    <h1 className='icon-name'>vegan</h1>
-                </div>
+                <IconContainer iconImage={icons[7].image} iconName='spicy' />
+                <IconContainer iconImage={icons[8].image} iconName='vegeterian' />
+                <IconContainer iconImage={icons[9].image} iconName='vegan' />
             </div>
         </div>
     );
